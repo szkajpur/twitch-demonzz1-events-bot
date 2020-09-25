ecd2d4d17e78a4df8b05578683c9a8f708bddd1c
szkajpur <70982115+szkajpur@users.noreply.github.com>
2020-09-25 23:46:58 +0200

    kolorki
    dodałem kolorki z biblioteki "chalk"

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ChatClient } = require("dank-twitch-irc");
+const chalk = require('chalk');
 const config = require("./config.json");
 let client = new ChatClient({
     username: config.username,
@@ -9,7 +10,7 @@ client.connect();
 client.joinAll(config.channels);
 
 client.on("ready", () => {
-    console.log(`Pomyślnie połączono do czatu: ${config.channels[0]}`);
+    console.log(chalk.greenBright("Pomyślnie połączono do czatu:") + chalk.blueBright(` ${config.channels[0]}`));
     client.say(config.channels[0], `${config.connect}`);
 });
 
@@ -20,15 +21,15 @@ client.on("PRIVMSG", message => {
     if (message.senderUsername == "demonzzbot"){
         if (message.messageText == "The arena has been cleaned up... Want to go again?! Type !boss to start!"){
             client.say(config.channels[0], "!boss");
-            console.log("Wysłano !boss");
+            console.log(chalk.yellowBright("Wysłano !boss"));
         }
         if (message.messageText == "The arena is now open! Type !ffa to join!"){
             client.say(config.channels[0], "!ffa");
-            console.log("Wysłano !ffa");
+            console.log(chalk.yellowBright("Wysłano !ffa"));
         }
         if (message.messageText == "The cops have given up! If you want to get a team together type !heist (amount)."){
             client.say(config.channels[0], `!heist ${config.heist}`);
-            console.log(`Wysłano !heist ${config.heist}`);
+            console.log(chalk.yellowBright(`Wysłano !heist ${config.heist}`));
         }
     }
 //demonzzbot events
