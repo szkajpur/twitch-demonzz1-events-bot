526a70a6873ea4860ae34750aea72949e4b220e3
szkajpur <70982115+szkajpur@users.noreply.github.com>
2022-02-21 21:31:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-const { ChatClient } = require("dank-twitch-irc");
+const { ChatClient, AlternateMessageModifier, SlowModeRateLimiter, replyToServerPing } = require('@aidenhadisi/amazeful-twitch-irc');
 const chalk = require('chalk');
 const config = require("./config.json");
+
+// declare client
 let client = new ChatClient({
     username: config.username,
-    password: config.oauth
+    password: config.oauth,
+    rateLimits: "default"
+});
+
+// events on client
+client.use(new AlternateMessageModifier(client));
+client.use(new SlowModeRateLimiter(client, 10));
+
+client.on("ready", async () => {
+	console.log(chalk.greenBright("Pomyślnie połączono do czatu:") + chalk.blueBright(` ${config.channels[0]}`));
+    client.say(config.channels[0], `${config.connect}`);
+});
+client.on("close", async (error) => {
+    if (error !== null){
+        console.error(`Client closed due to error`, error);
+    }
+});
+
+//demonzzbot events
+client.on("PRIVMSG", async (msg) => {
+    if (msg.senderUsername == client.configuration.username){
+        return;        
+    }
+    if (msg.senderUserID == '180654261'){
+        if (msg.messageText.includes("Type !boss to join!")){
+            client.say(msg.channelName, "!boss");
+        }
+        if (msg.messageText.includes("Type !ffa to join!")){
+            client.say(msg.channelName, "!ffa");
+        }
+        if (msg.messageText.includes("-Everyone can Join!- In order to join type !heist (amount).")){
+            client.say(msg.channelName, `!heist ${config.heist}`);
+        }
+    };
+
+//sprawdzanie czy bot dziala
+    if (msg.messageText.startsWith(config.prefix)){
+        const komenda = msg.messageText.slice(config.prefix.length).split(" ")[0];
+        switch(komenda){
+            case `${config.command}`:
+                client.say(msg.channelName, `@${message.senderUsername}, Bot działa prawidłowo ;)`);
+                break;
+        }
+    }
 });
 
 client.connect();
 client.joinAll(config.channels);
-
-client.on("ready", () => {
-    console.log(chalk.greenBright("Pomyślnie połączono do czatu:") + chalk.blueBright(` ${config.channels[0]}`));
-    client.say(config.channels[0], `${config.connect}`);
-});
-
-client.on("PRIVMSG", message => {
-    if (message.senderUsername == client.configuration.username){
-        return;        
-    }
-    if (message.senderUsername == "demonzzbot"){
-        if (message.messageText.includes("Type !boss to join!")){
-            client.say(config.channels[0], "!boss");
-            console.log(chalk.yellowBright("Wysłano !boss"));
-        }
-        if (message.messageText.includes("Type !ffa to join!")){
-            client.say(config.channels[0], "!ffa");
-            console.log(chalk.yellowBright("Wysłano !ffa"));
-        }
-        if (message.messageText.includes("-Everyone can Join!- In order to join type !heist (amount).")){
-            client.say(config.channels[0], `!heist ${config.heist}`);
-            console.log(chalk.yellowBright(`Wysłano !heist ${config.heist}`));
-        }
-    }
-//demonzzbot events
-
-    if (message.messageText.startsWith(config.prefix)){
-        const komenda = message.messageText.slice(config.prefix.length).split(" ")[0];
-        switch(komenda){
-            case `${config.command}`:
-                client.say(message.channelName, `@${message.senderUsername}, Bot działa prawidłowo ;)`);
-                break;
-        }
-    }
-//sprawdzanie czy bot dziala
-})
